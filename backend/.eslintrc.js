6af7aa240a9273ceac2b1e4a6d8311f634d97012
JonesBass <jonesbass@pop-os.localdomain>
2022-07-16 21:26:48 -0300

    refatorando e criando usuarios

---
+++ backend/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier'],
+
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    'class-methods-use-this': 'off',
+    '@typescript-eslint/camelcase': 'off',
+    'no-useless-constructor': 'off',
+  },
+};
