5e96b5bbbf91f2fcf8d8e5ff531e06f8e2ea93a6
JonesBass <jonesbass@pop-os.localdomain>
2022-07-19 22:23:41 -0300

    funcionalidades de agendamentos

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -7,6 +7,9 @@ import SessionController from './app/controllers/SessionController';
 
 import authMiddleware from './app/middlewares/auth';
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 const routes = new Router();
 const uploads = multer(multerConfig);
@@ -17,6 +20,12 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+routes.get('/providers', ProviderController.index);
+
+routes.get('/appointment', AppointmentController.index);
+routes.post('/appointment', AppointmentController.store);
+
+routes.get('/schedule', ScheduleController.index);
 
 routes.post('/files', uploads.single('file'), FileController.store);
 
