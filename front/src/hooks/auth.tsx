53d88f61f985b478dbc601210964b9424a22660d
jones-bass <jonesbass.tb@gmail.com>
2023-04-13 11:13:34 -0300

    localStorage e Logout

--- front/src/hooks/auth.tsx
+++ front/src/hooks/auth.tsx
@@ -1,5 +1,17 @@
-import React, { useCallback, useContext, createContext } from 'react'
+import React, { useCallback, useContext, createContext, useState } from 'react'
 import { api } from '../services/api'
+
+interface User {
+  id: string
+  name: string
+  email: string
+  avatar_url: string
+}
+
+interface AuthState {
+  token: string
+  user: User
+}
 
 interface SignInCredentials {
   email: string
@@ -7,6 +19,8 @@ interface SignInCredentials {
 }
 
 interface AuthContextData {
+  user: User
+  signOut(): void
   signIn(credentials: SignInCredentials): Promise<void>
 }
 
@@ -16,13 +30,42 @@ export interface IAuthContextData {
 const AuthContext = createContext<AuthContextData>({} as AuthContextData)
 
 export const AuthProvider = ({ children }: IAuthContextData) => {
+  const [data, setData] = useState<AuthState>(() => {
+    const token = localStorage.getItem('@GoBarber:token')
+    const user = localStorage.getItem('@GoBarber:user')
+
+    if (token && user) {
+      api.defaults.headers.authorization = `Bearer ${token}`
+      return { token, user: JSON.parse(user) }
+    }
+
+    return {} as AuthState
+  })
+
   const signIn = useCallback(async ({ email, password }: SignInCredentials) => {
     const response = await api.post('sessions', { email, password })
     console.log(response.data)
+
+    const { token, user } = response.data
+    localStorage.setItem('@GoBarber:token', token)
+    localStorage.setItem('@GoBarber:user', JSON.stringify(user))
+
+    api.defaults.headers.authorization = `Bearer ${token}`
+
+    setData({ token, user })
+  }, [])
+
+  const signOut = useCallback(() => {
+    localStorage.removeItem('@GoBarber:token')
+    localStorage.removeItem('@GoBarber:user')
+
+    setData({} as AuthState)
   }, [])
 
   return (
-    <AuthContext.Provider value={{ signIn }}>{children}</AuthContext.Provider>
+    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
+      {children}
+    </AuthContext.Provider>
   )
 }
 
